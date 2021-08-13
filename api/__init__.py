056f56eb20c7f6a66197a1d69c37332ea00a8ed0
Zach Cyrus <zscyrus31@gmail.com>
2021-08-13 17:22:02 -0400

    Reconfigured init function

--- api/__init__.py
+++ api/__init__.py
@@ -2,18 +2,37 @@ import os
 
 from flask import Flask
 
-app = Flask(__name__)
+# Application factory
+def create_app(testing=False):
+    app = Flask(__name__)
 
-# Index route which will return Hello World.
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+    # Logic for handling configuration
+    if app.config["ENV"] == 'production':
+        app.config.from_object("config.ProductionConfig")
 
-# Importing modules
-from . import user
-from .routes import exercises
+    elif app.config["ENV"] == 'testing':
+        app.config.from_object("config.TestingConfig")
+
+    else:
+        app.config.from_object("config.DevelopmentConfig")
 
 
-# Registering blueprint routes
-app.register_blueprint(user.bp)
-app.register_blueprint(exercises.bp)
+    # Index route which will return Hello World.
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    # Importing modules
+    from . import user
+    from .routes import exercises
+
+
+    # Registering blueprint routes
+    app.register_blueprint(user.bp)
+    app.register_blueprint(exercises.bp)
+
+    return app
+
+
+
+

89f363ec13532b451086c986391dc6253fafb2a9
Zach Cyrus <zscyrus31@gmail.com>
2021-08-13 17:24:20 -0400

    Added a configuration file

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
 class DevelopmentConfig(Config):
     ENV = "development"
     DEVELOPMENT = True
+
+class ProductionConfig(Config):
+    ENV = "production"
+
+class TestingConfig(Config):
+    TESTING = True
+    # Enter DB credentials later when it is implemented

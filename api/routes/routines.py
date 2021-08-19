9bc97f92a211dde8005653833aa4b34798862b3b
Zach Cyrus <zscyrus31@gmail.com>
2021-08-19 11:29:05 -0400

    Added remove routine method and add routine method

--- api/routes/routines.py
+++ api/routes/routines.py
@@ -1,6 +1,7 @@
-# Route for defining routines endpoints 
+# Route for defining routines endpoints
 
 from flask import (Blueprint, request)
+import random
 
 bp = Blueprint('routines', __name__, url_prefix='/routines')
 
@@ -8,26 +9,62 @@ user_routines = [
     {
         "id": 11,
         "routine_name": "Zach's PPL Routine",
-        "exercises": []
+        "exercises": [],
+        "routine_id":342
     },
     {
         "id": 10,
         "routine_name": "GZCL Method Routine",
-        "exercises": []
+        "exercises": [],
+        "routine_id":341
     }
 ]
+
 
 @bp.route('/', methods=["GET"])
 def get_routines():
     # possibly return all user submitted routines
-    return {"all_routines":user_routines}
+    return {"all_routines": user_routines}
+
 
 @bp.route('/add', methods=["POST"])
 def add_routines():
     if request.method != 'POST' or not(request.is_json):
         return {
-            "error": "Request must be a POST method, and request must be JSON"
+            "error": "Request must be a POST method, and request must contain JSON"
         }, 400
+
+    elif ('id' or "routine_name" or "exercises") not in request.json:
+        return {
+            "error": "Request must contain a id, routine_name, and exercises"
+        }, 400
+
     else:
-        new_routine = request.json["routine"]
+        new_routine = request.json
+        new_routine["routine_id"] = random.randint(343, 1103)
+        user_routines.append(new_routine)
+        return {
+            "success": new_routine["routine_name"] + "was successfully added!",
+            "data": user_routines
+
+        }, 200
+
+
+@bp.route('/remove/<int:routine_id>', methods=["DELETE"])
+def remove_routine(routine_id):
+    if request.method != "DELETE":
+        return {
+            "error": "This is a delete only route"
+        }
+    else:
+        # have to loop through array of dicts
+        for index, routine in enumerate(list(user_routines)):
+            if routine_id == routine["routine_id"]:
+                del user_routines[index]
+                return {
+                    "success": "Resource was successfully deleted"
+                }, 200
         
+        return {
+            "error": "Routine id was not found"
+        }, 400

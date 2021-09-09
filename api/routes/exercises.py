18faab83375e20c5ed7f3d698e155571ab2b9a21
Zach Cyrus <zscyrus31@gmail.com>
2021-09-09 12:51:12 -0400

    Added route to add exercise to particular routine

--- api/routes/exercises.py
+++ api/routes/exercises.py
@@ -12,37 +12,73 @@ exercise_list = [
 
 
 from flask import (Blueprint, request)
+from api.models.models import User, Routine_Exercises, Exercises
 
 bp = Blueprint('exercise', __name__, url_prefix='/exercise')
 
 @bp.route('/', methods=["GET"])
 def get_exercises():
-    # flask apparently returns python dicts as json automatically
-    return {"all_exercises":exercise_list}
-
-@bp.route('/add', methods=["POST"])
-def add_exercise():
+    return {"all_exercises":"testing"}
 
 
-    '''
-
-    Format for data that should be posted
-    {
-        exercise_name: "Sample"
-    }
-
-    '''
-
+# Route to add exercise to a particular routine
+@bp.route('/add/<int:user_id/<string:routine_name>', methods=["POST"])
+def add_exercise_routine(user_id,routine_name):
 
     if request.method != 'POST' or not(request.is_json):
         return {
-            "error": "Request must be a POST method, and request must be JSON"
+            "error": "Request must be a POST method, and request body must contain JSON"
         }, 400
-    else:
-        new_exercise = request.json["exercise_name"]
-        exercise_list.append(new_exercise)
 
+    elif ('exercise_name' or 'exercise_description') not in request.json:
         return {
-            "success": new_exercise + " was successfully added",
-            "exercise_list":exercise_list
-        }, 200
+            "error": "Request must contain exercise_name and exercise_description within the body"
+        }, 400
+
+    elif User.find_user_by_id(user_id) is False:
+        return {
+            "error": "User with that id doesn't exist"
+        }, 400
+
+    # Find routine with that name associated with that user
+
+    else:
+        current_user = User.find_user_by_id(user_id)
+
+        current_user_routine = current_user.find_routine(routine_name)
+
+        if current_user_routine is False:
+            return {
+                "error": "Routine not found associated with that user_id"
+            }, 400
+
+        # Add exercise to exercise table
+
+        try:
+             exercise_to_add = Exercises(request.json)
+
+             exercise_to_add.save_exercise()
+
+             added_exercise_id = exercise_to_add.exercise_id
+
+             # Now to save exercise to user_routine table
+
+             new_routine_exercise = Routine_Exercises(reps=10,exercise_id=added_exercise_id,routine_id=current_user_routine.routine_id)
+
+             new_routine_exercise.save_routine_exercises()
+
+             return {
+                 "routine_name": routine_name,
+                 "exercise_name": exercise_to_add.exercise_name,
+                 "reps": new_routine_exercise.reps
+
+             },200
+
+
+
+            
+        except Exception as e:
+            return {
+                "error": "Error creating routine table please try again",
+                "details":e
+            },400

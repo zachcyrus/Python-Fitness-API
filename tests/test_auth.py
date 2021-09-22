d0dfa8785367e83d87fbc29e403435209cf2a484
Zach Cyrus <zscyrus31@gmail.com>
2021-09-22 12:33:39 -0400

    Changed fixture in the auth route

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 from api import create_app, db as _db
 import json
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def client():
     app = create_app(testing=True)
     
@@ -40,3 +40,20 @@ class TestAuthRoute:
         assert type(signup_response_data["password"]) is str
 
         assert type(signup_response_data['user_id']) is int
+
+
+    def test_login_route(self, client):
+        '''Test login route for a registered user'''
+
+        login_response = client.post(
+            '/api/auth/login',
+            data = json.dumps({
+                "user_name": "Test_UserAB",
+                "password":"testpass90"
+            }),
+            content_type='application/json'
+        )
+
+        assert login_response.status_code == 200
+
+        
